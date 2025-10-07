39677e11fb7bfea4a0167094a84be71974c3c248
Veillard10 <202502857012@alunos.estacio.br>
2025-10-07 05:16:05 -0300

    logica_novato

--- .vscode/super_trunfo.c
+++ .vscode/super_trunfo.c
@@ -2,6 +2,12 @@
 
 int main () { 
 
+    //Dados das cartas
+
+    int Carta_carta1 = {"São Paulo", "SP"};
+    int Carta_carta2 = {"Rio de Janeiro","RJ"};
+
+
     char Estado1[4],estado2[4];
     char Codigo_carta1[4], codigo_carta2[4];
     char Nomecidade[49],Nomecidade2[49];
@@ -45,25 +51,89 @@ int main () {
     scanf("%f",&Pib2);
     printf("Informe a quantidade de pontos turísticos:\n");
 scanf("%d",&PontosTuristicos2);
+
+
+
+// Calcular Densidade Populacional e PIB per capita
+
+printf("Calculadora de Indicadores Socioeconômicos\n");
+
+// Entrada de Dados carta 1
+
+printf("Digite a População:\n");
+scanf("%d",&População1);
+
+printf("Digite a Area:\n");
+scanf("%f",&Area1);
+
+printf("Digite o Pib:\n");
+scanf("%f",&Pib1);
+
+//Saída de Dados
+
+if(População1 = 11904961){
+printf("Densidade Populacional: %.2f hab\n");
+}else{
+printf("Densidade Populacional: não calculada");
+}
+if(Area1 = 152111){
+printf("Area: %.2f km²\n");
+}else{
+printf("Area: Kilometragem não calculada\n");        
+}
+if(Pib1 = 3.13){
+printf("Pib per capita: %.2f Trilhões\n");
+}else{
+printf("Pib per capita: não calculado\n");
 }
 
-int main{
-int População1;
-float Area1;
 
-printf("")
-printf("Digite o valor da População 1");
-scanf("%d",População1);
+// Entrada de Dados carta 2
 
-printf("Digite o valor de Area 1");
-scanf("%f",Area1);
+printf("Digite a População:\n");
+scanf("%d",&População2);
 
-printf("Calculo ")
+printf("Digite a Area:\n");
+scanf("%f",&Area2);
 
-if(Area1 > População);
-print("Area 1 é maior que Area 2!"\n);
+printf("Digite o Pib:\n");
+scanf("%f",&Pib2);
+
+//Saída de Dados
+
+if(População2 = 17223547){
+printf("Densidade Populacional: %.2f hab\n");
+}else{
+printf("Densidade Populacional: não calculada");
+}
+if(Area2 = 1200329){
+printf("Area: %.2f km²\n");
+}else{
+printf("Area: Kilometragem não calculada\n");        
+}
+if(Pib2 = 1.15){
+printf("Pib per capita: %.2f Trilhões\n");
+}else{
+printf("Pib per capita: não calculado\n");
+}
+
+// Atributo de comparação (PIB) 
+
+float Pib1 = 3.13;
+float Pib2 = 1.15;
+
+printf("Comparação de Cartas(Atributo: PIB)");
+
+printf("Carta 1 (SP):3.13 Trilhões\n")
+printf("Carta 2 (RJ):1,15 Trilhões\n")
+Printf("Carta 1 (São Paulo) Venceu!")
+
+
 
 
 }
+
+
+
 return 0;
 }
